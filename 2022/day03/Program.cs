03c1f64bd2316197193a70918168bb10d5990d95
El Zubeir Huweidi <zubeir.huweidi@gmail.com>
2023-11-06 23:33:14 +0100

    license

--- 2022/day03/Program.cs
+++ 2022/day03/Program.cs
@@ -1,24 +1,16 @@
-﻿
-// 1 item per rucksack
+﻿using System.Linq;
 
+var input = File.ReadAllText("input.txt");
 
-class Program
-{
-    static void Main()
-    {
-        var input = File.ReadAllText("input.txt");
-        Console.WriteLine(input);
-
-
-
-
+var part1 = input.Lines
+    .Select(x => x.Batch(x.Length / 2))
+    .Select(x => x.First().Intersect(x.Last()))
+    .SelectMany(x => x)
+    .Select(x => char.IsLower(x) ? x - 'a' + 1 : x - 'A' + 27)
+    .Sum()
+    .ToString();
 
 
 
 
-
-
-
-    }
-}
 
