756853f9ce8eddd151191d14809ff8af4e2d6998
El Zubeir Huweidi <zubeir.huweidi@gmail.com>
2024-01-01 00:46:04 +0100

    go go 3

---
+++ 2023/day03/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"strings"
+
+	"github.com/zizouhuweidi/advent-of-code/utils"
+)
+
+func main() {
+	data := strings.Split(strings.Trim(utils.GetInput("input.txt"), "\n"), "\n")
+}
